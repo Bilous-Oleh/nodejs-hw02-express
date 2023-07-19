67e1842a66df6a03bcd0f3959935f88ffc1fc175
Bilous-Oleh <2005net2005@gmail.com>
2023-07-19 23:41:34 +0300

    Create db connect

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-  // console.log("Server running. Use our API on port: 3000")
-});
+const { DB_HOST, PORT } = process.env;
+
+// const DB_HOST =
+//   "mongodb+srv://Oleh:NR0rpheh0BXRsWt0@cluster0.lp1jmxp.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connection successful"))
+//   .catch((error) => console.log(process.exit(1)));
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
